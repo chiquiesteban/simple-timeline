cb4f66dee47749b3d9789273de1f1e146ea79a98
Chiqui <chiquiesteban@gmail.com>
2013-04-29 18:18:04 -0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,9 @@
+var events = [
+  ["Note", "Headline", "04/10/2013 10:45:00", "important", "Text", "Multimedia content","Link", "Linked content"],
+	["Note", "Headline", "04/16/2013 18:45:00", "", "Text", "Multimedia content","Link", "Linked content"],
+	["Note", "Headline", "05/15/2013 13:45:00", "", "Text", "Multimedia content","Link", "Linked content"],
+	["Note", "Headline", "06/25/2013 14:00:00", "important", "Text", "Multimedia content","Link", "Linked content"],
+	["Note", "Headline", "06/27/2013 18:45:00", "", "Text", "Multimedia content","Link", "Linked content"],
+	["Note", "Headline", "10/15/2013 14:45:00", "", "Text", "Multimedia content","Link", "Linked content"],
+	["Note", "Headline", "10/15/2013 20:45:00", "important", "Text", "Multimedia content","Link", "Linked content"],
+];
